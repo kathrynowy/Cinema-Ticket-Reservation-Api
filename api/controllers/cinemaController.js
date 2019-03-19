26ea6feff98aacb3c1f5e06186cece5e47a68c75
katerina <katuha_maksimova@mail.ru>
2019-03-19 13:59:28 +0300

    replace err with error, delete unnecessary code

--- api/controllers/cinemaController.js
+++ api/controllers/cinemaController.js
@@ -1,60 +1,52 @@
-const mongoose = require('mongoose'),
-  Cinema = mongoose.model('Cinema');
+const mongoose = require('mongoose');
+Cinema = mongoose.model('Cinema');
 
 
 function listСinemas(req, res) {
-  Cinema.find({}, function (err, cinemas) {
-    if (err) {
-      res.send(err);
+  Cinema.find({}, function (error, cinemas) {
+    if (error) {
+      res.send(error);
     }
     res.json(cinemas);
-
   });
 };
 
 function readСinema(req, res) {
-  Cinema.findById(req.params.id, function (err, cinema) {
-    console.log(req.params.id);
-    if (err) {
-      res.send(err);
+  Cinema.findById(req.params.id, function (error, cinema) {
+    if (error) {
+      res.send(error);
     }
     res.json(cinema);
   });
 };
 
 function createСinema(req, res) {
-  const new_cinema = new Cinema(req.body);
-  new_cinema.save(function (err, cinema) {
-    console.log(req.params.id);
-    if (err) {
-      res.send(err);
+  const newСinema = new Cinema(req.body);
+  newСinema.save(function (error, cinema) {
+    if (error) {
+      res.send(error);
     }
     res.json(cinema);
   });
 };
 
 function updateCinema(req, res) {
-  Cinema.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, cinema) {
-    if (err) {
-      res.send(err);
+  Cinema.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (error, cinema) {
+    if (error) {
+      res.send(error);
     }
     res.json(cinema);
   });
 };
 
 function deleteСinema(req, res) {
-  Cinema.remove({
-    _id: req.params.id
-  }, function (err, cinema) {
-    console.log(req.params.id);
-    if (err) {
-      res.send(err);
+  Cinema.remove({ id: req.params.id }, function (error, cinema) {
+    if (error) {
+      res.send(error);
     }
     res.json({ message: 'Cinema successfully deleted' });
   });
 };
-
-
 
 module.exports = {
   listСinemas,
