0121d0a1ec21070275cd93e0f44a8e0822c5df2e
katerina <katuha_maksimova@mail.ru>
2019-04-17 20:44:43 +0300

    connect to atlas db

--- api/utils/DataBaseUtils.js
+++ api/utils/DataBaseUtils.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../../config.json');
 
 function setUpConnection() {
-  mongoose.connect(`mongodb://${config.db[0].host}/${config.db[0].name}`, { useNewUrlParser: true });
+  mongoose.connect(/* `mongodb://${config.db[0].host}/${config.db[0].name}` */ `mongodb+srv://kathrynowy:katerina99@cluster0-lawht.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true });
   return mongoose.connection;
 }
 
